518ee0ec2a9ab311f3b6e4ab24c83dcc777a9d1e
Yume <b1018013@fun.ac.jp>
2021-10-19 16:38:32 +0900

    InterfaceとRepositoryの修正・追加

--- app/src/main/java/com/example/hotpepper_api/ResultRepository.kt
+++ app/src/main/java/com/example/hotpepper_api/ResultRepository.kt
@@ -1,13 +1,22 @@
 package com.example.hotpepper_api
 
+import com.example.hotpepper_api.model.ResultResponse
 import com.squareup.moshi.Moshi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.io.IOException
 
 class ResultRepository {
-    fun getRetrofit() = Retrofit.Builder()
+    private val retrofit = Retrofit.Builder()
             .baseUrl(HotpepperApi.baseUrl)
             .addConverterFactory(MoshiConverterFactory.create(Moshi.Builder().build()))
             .build()
-            .create(HotpepperApi::class.java)
+
+    private val hotPepperService = retrofit.create(HotpepperApi::class.java)
+
+    suspend fun getResultResponse(key: String, small_area: String, format: String): ResultResponse {
+        return hotPepperService.getResultResponse(key,small_area, format)
+    }
 }
