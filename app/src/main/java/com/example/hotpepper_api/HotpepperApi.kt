840733febc3288e20143b4b39dafd732b8cc8a6a
Yume <b1018013@fun.ac.jp>
2021-10-21 15:47:26 +0900

    URLの消去

--- app/src/main/java/com/example/hotpepper_api/HotpepperApi.kt
+++ app/src/main/java/com/example/hotpepper_api/HotpepperApi.kt
@@ -1,15 +1,13 @@
 package com.example.hotpepper_api
 
-import com.example.hotpepper_api.model.response.ResultResponse
+import com.example.hotpepper_api.model.ResultResponse
 import retrofit2.Call
 import retrofit2.http.GET
-import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface HotpepperApi {
-    companion object {
-        const val baseUrl = "http://webservice.recruit.co.jp/hotpepper/gourmet/v1"
-    }
     @GET("/")
-    fun getResult(@Query("key") key: String, @Query("small_area") small_area: String, @Query("format") format: String): Call<ResultResponse>
+    suspend fun getResultResponse(@Query("key") key: String,
+                                  @Query("small_area") small_area: String,
+                                  @Query("format") format: String): Call<ResultResponse>
 }
