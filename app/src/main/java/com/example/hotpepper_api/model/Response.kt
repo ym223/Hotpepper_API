c108c8eefa766ba40b77990aa1cd04c7cf988168
Yume <b1018013@fun.ac.jp>
2021-10-25 03:01:07 +0900

    モデルの修正

--- app/src/main/java/com/example/hotpepper_api/model/Response.kt
+++ app/src/main/java/com/example/hotpepper_api/model/Response.kt
@@ -16,39 +16,39 @@ data class Response(
     ){
         @JsonClass(generateAdapter = true)
         data class Shop(
-                val access: String?,
-                val address: String?,
-                val budget_memo: String?,
-                val catch: String?,
-                val genre: Genre?,
-                val id: String?,
-                val lat: Double?,
-                val lng: Double?,
-                val name: String?,
-                val other_memo: String?,
-                val photo: Photo?,
-                val shop_detail_memo: String?,
-                val urls: Urls?
+                val access: String,
+                val address: String,
+                val budget_memo: String,
+                val catch: String,
+                val genre: Genre,
+                val id: String,
+                val lat: Double,
+                val lng: Double,
+                val name: String,
+                val other_memo: String,
+                val photo: Photo,
+                val shop_detail_memo: String,
+                val urls: Urls
         ){
             @JsonClass(generateAdapter = true)
             data class Genre(
-                    val catch: String?,
-                    val name: String?
+                    val catch: String,
+                    val name: String
                     )
             @JsonClass(generateAdapter = true)
             data class Photo(
-                    val pc: Pc?
+                    val pc: Pc
             ){
                 @JsonClass(generateAdapter = true)
                 data class Pc(
-                        val l: String?,
-                        val m: String?,
-                        val s: String?
+                        val l: String,
+                        val m: String,
+                        val s: String
                 )
             }
             @JsonClass(generateAdapter = true)
             data class Urls(
-                    val pc: String?
+                    val pc: String
             )
         }
     }
